ff40c7e3970472dd8e06e102241cc4aa07ab20fc
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-09 15:15:35 +0000

    index.php 1st iter
    Signed-off-by: Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php
+    $view = new stdClass(); //creating the view
+    $view->pageTitle = 'Please Login'; //giving tab a name
+    //require_once('Models/LoginValidator.php');
 
-    //this is a comment, i wish I had more of these in my cliserv project
+    //session is started
+    session_start();
 
-    //require the LoginValidator class
-    
+    //check if login button has been clicked
+    if(isset($_POST['submit']))
+    {
+        //username and password set to what was input, will be passed to method later
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //Once button is clicked, grab user data from the loginValidator class
+        //$loginValidator = new LoginValidator();
+        //$loginDetails = $loginValidator->logUserIn($username,$password)
+        //if no details, that means the details input were incorrect
+        //if(!$loginDetails)
+        //{
+            //if details where entered incorrectly, output error message
+            //$view->loginError = "Incorrect Login Details, please try again";
+        //}
+        //otherwise, set session user details to be
+        //else
+        //{
+            //session has variables set to indicate a user has logged in
+            //$_SESSION['userID'] = $logUserIn['userID'];
+            //$_SESSION['userName'] = $logUserIn['userName'];
+            //$_SESSION['userName'] = $logUserIn['userName'];
+            //$_SESSION['userName'] = $logUserIn['userName'];
+            //$_SESSION['userName'] = $logUserIn['userName'];
+            //$_SESSION['userName'] = $logUserIn['userName'];
+            //header("Location: index.php");
+        //}
+    }
+
+    require("Views/index.phtml");
