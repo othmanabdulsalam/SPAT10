00ee80de0fad373f70536001a2756014a85371c1
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-09 17:43:28 +0000

    uncommenting index.php
    Signed-off-by: Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $view = new stdClass(); //creating the view
     $view->pageTitle = 'Please Login'; //giving tab a name
-    //require_once('Models/LoginValidator.php');
+    require_once('Models/LoginValidator.php');
 
     //session is started
     session_start();
@@ -10,30 +10,23 @@
     if(isset($_POST['submit']))
     {
         //username and password set to what was input, will be passed to method later
-        $username = $_POST['username'];
+        $usernameOrEmail = $_POST['username'];
         $password = $_POST['password'];
 
         //Once button is clicked, grab user data from the loginValidator class
-        //$loginValidator = new LoginValidator();
-        //$loginDetails = $loginValidator->logUserIn($username,$password)
+        $loginValidator = new LoginQuery();
+        $userDetails = $loginValidator->validate($usernameOrEmail,$usernameOrEmail,$password);
         //if no details, that means the details input were incorrect
-        //if(!$loginDetails)
-        //{
+        if(!$userDetails)
+        {
             //if details where entered incorrectly, output error message
-            //$view->loginError = "Incorrect Login Details, please try again";
-        //}
-        //otherwise, set session user details to be
-        //else
-        //{
-            //session has variables set to indicate a user has logged in
-            //$_SESSION['userID'] = $logUserIn['userID'];
-            //$_SESSION['userName'] = $logUserIn['userName'];
-            //$_SESSION['userName'] = $logUserIn['userName'];
-            //$_SESSION['userName'] = $logUserIn['userName'];
-            //$_SESSION['userName'] = $logUserIn['userName'];
-            //$_SESSION['userName'] = $logUserIn['userName'];
-            //header("Location: index.php");
-        //}
+            $view->loginError = "Incorrect Login Details, please try again";
+        }
+        else
+        {
+            //session has been set, refresh page where user will be logged in and their options displayed
+            header("Location: index.php");
+        }
     }
 
     require("Views/index.phtml");
