f74938dbe32cfeeeccf8154b13b34369a5741b8b
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-10 12:42:01 +0000

    logout comments
    Signed-off-by: Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>

--- Controllers/logout.php
+++ Controllers/logout.php
@@ -1,4 +1,12 @@
 <?php
+    /**
+     * Logout Controller by Othman Abdulsalam
+     *
+     * Whenever logout button in the header is clicked, link to this controller
+     * is activated
+     *
+     * All session details are destroyed and index is loaded to the login part
+     */
 
     //session is started,session details are still saved
     session_start();
