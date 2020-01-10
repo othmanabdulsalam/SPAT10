6a56ca8f6c3a45c339963858bf98784d342209f3
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-10 16:12:57 +0000

    report controller should now convert
    Signed-off-by: Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>

--- Controllers/report.php
+++ Controllers/report.php
@@ -2,27 +2,29 @@
     /**
      * Report Controller By Othman Abdulsalam
      *
-     * When a client requests to see a report, a page is loaded with
-     * the correct report displaying.
+     * When a client requests to see a report, a pop up appears allowing
+     * the pdf to be downloaded anywhere in storage
      */
 
-    $view = new stdClass(); //creating the view
-    //$view->pageTitle = 'Please Login'; //giving tab a name
 
+    //require the autoloader from dompdf
+    require_once('dompdf/autoload.inc.php');
 
+    //reference dompdf namespace
+    use Dompdf\Dompdf;
 
+    //instantiate dompdf object
+    $dompdf = new Dompdf();
 
+    //load the content of the report.phtml
+    $report = file_get_contents("../Views/report.phtml");
+    $dompdf->loadHtml($report);
 
+    //setup report size
+    $dompdf->setPaper('A4','portrait');
 
+    //rendered as PDF
+    $dompdf->render();
 
-
-
-
-
-
-
-
-
-
-    //$view->pageTitle = 'Please Login'; //giving tab a name
-    require('../Views/report.phtml');
+    //PDF is now output, pop up appears because of value "1"
+    $dompdf->stream("Audit Report", array("Attachment" => 1));
