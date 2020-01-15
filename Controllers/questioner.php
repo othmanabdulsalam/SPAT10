3da5cf05454c451c8bcc35adbcb13483666720b1
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-15 12:07:22 +0000

    Signed-off-by: Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>

--- Controllers/questioner.php
+++ Controllers/questioner.php
@@ -1,6 +1,24 @@
 <?php
-    $view = new stdClass();
+    /**
+     * questioner.php created by Othman Abdulsalam
+     *
+     * This controller handles the grabbing of a selected incomplete audit
+     * for the questioner to work on
+     */
 
-    $audit = $_GET;
+    $view = new stdClass(); //creating the view
+    $view->pageTitle = 'Questions'; //giving tab a name
+    require_once('Models/AuditQuery.php'); //for querying the audits that need to be displayed
 
-    var_dump($audit);
+    session_start();//start session
+    $auditID = $_GET;
+
+    $auditQuery = new AuditQuery();//create AuditQuery object
+    $incompleteAudit = $auditQuery->getInProgressAudit($auditID);//grab the specific audit the questioner wants to complete
+
+    $view->incompleteAudit = $incompleteAudit;
+
+    //require the questioner page
+    //require_once('../Views/questioner.phtml');
+
+
