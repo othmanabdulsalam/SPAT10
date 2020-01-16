565ca56cda3ea7108e519425708cd854faeac929
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-16 15:44:30 +0000

    createAudit controller

---
+++ Controllers/createAudit.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * createAudit controller by Othman Abdulsalam
+     *
+     * This controller will generate a client's audit that
+     * questioners and scorers will work on before
+     * it is completed and to be viewed by the client
+     */
+
+    $view = new stdClass(); //creating a view
+    $view->pageTitle = 'Create a new audit'; //giving tab a name
+    require_once('../Models/UserQuery.php');//require the userQuery class
+
+    session_start();//start session
+
+    
