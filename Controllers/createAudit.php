fcbd8f8abf8aa692ba342ee5d63f9ddf4fc9a584
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com>
2020-01-17 12:13:22 +0000

    daddy

--- Controllers/createAudit.php
+++ Controllers/createAudit.php
@@ -12,34 +12,36 @@
     require_once('../Models/AuditCreationQuery.php');//require the userQuery class
 
     session_start();//start session
-$auditCreationQuery = new AuditCreationQuery();//create AuditQuery object
-$auditInfo = $auditCreationQuery->getAllAuditCreationInfo();//grab the specific audit the questioner wants to complete
-$view->auditInfo = $auditInfo;
+    $auditCreationQuery = new AuditCreationQuery();//create AuditQuery object
+    $auditInfo = $auditCreationQuery->getAllAuditCreationInfo();//grab the specific audit the questioner wants to complete
+    $view->auditInfo = $auditInfo;
 
     //check if create audit button is set
-    //if(isset($_POST['createAudit']))
-    //{
+    if(isset($_POST['createAudit']))
+    {
         //set audit's values
-        //$clientID = $_POST['clientID'];
-        //$questionIDArray = [];
-        //$questionIDCount = $_POST['questionIDCount'];
-        //for($i=0;$i<=$questionIDCount;$i++)
-        //{
-            //$questionID = $_POST['questionID'.$i];
-            //array_push($questionIDArray,$questionID);
-        //}
-        //$questionFlagArray = []
-        //$questionFlagCount = $_POST['questionFlagCount'];
-        //for($i=0;$i<=$questionFlagCount;$i++)
-        //{
-            //$questionFlag = []
-            //$questionFlag['questionID'] = $_POST['questionID'.$i];
-            //$questionFlag['flagID'] = $_POST['flagID'.$i];
-            //array_push($questionFlagArray,$questionFlag);
-        //}
-        //$location = $_POST['location'];
-        //$auditCreationQuery = new AuditCreationQuery();
-        //$auditCreationQuery->submitAudit($clientID,$location,$questionIDArray,$questionFlagArray);
-        //header('Location: ../index.php');
-    //}
+        $clientID = $_POST['clientID']; //grab clientID
+        $questionIDArray = [];//fill array of questionIDs
+        $questionIDCount = $_POST['questionIDCount'];
+        for($i=0;$i<=$questionIDCount;$i++)
+        {
+            $questionID = $_POST['questionID'.$i];
+            array_push($questionIDArray,$questionID);
+        }
+        $questionFlagArray = [];//fill array of question flags
+        $questionFlagCount = $_POST['questionFlagCount'];
+        for($i=0;$i<=$questionFlagCount;$i++)
+        {
+            $questionFlag = [];
+            $questionFlag['questionID'] = $_POST['questionID'.$i];
+            $questionFlag['flagID'] = $_POST['flagID'.$i];
+            array_push($questionFlagArray,$questionFlag);
+        }
+        $location = $_POST['location'];//grab the location
+        $auditCreationQuery = new AuditCreationQuery();
+        //pass the information to the database so an audit has been created
+        $auditCreationQuery->submitAudit($clientID,$location,$questionIDArray,$questionFlagArray);
+        //load the index.php page
+        header('Location: ../index.php');
+    }
 require_once('../Views/createAudit.phtml');
